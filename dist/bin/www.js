6d03a68bcf5b37848a5ba3aac1f75c6e92c07fbc
lqs5858@163.com <365858>
2021-01-01 15:39:47 +0800

    1.0.4

--- dist/bin/www.js
+++ dist/bin/www.js
@@ -5,6 +5,10 @@ const program = require('commander');
 
 const path = require('path');
 
-program.version(`Version is ${require(path.resolve(__dirname, '../../package.json')).version}`).description('A simple CLI for building initialize project include Wechat applet, Vue, Egg (nodejs)').usage('<command> [options]').command('create').action((name, cmd) => {
+const {
+  version
+} = require('../../package.json');
+
+program.version(version, '-V --version').description('A simple CLI for building initialize project include Wechat applet, Vue, Egg (nodejs)').usage('<command> [options]').command('create').action((name, cmd) => {
   require('../lib/create');
 }).parse(process.argv);
--- src/bin/www.js
+++ src/bin/www.js
@@ -1,9 +1,10 @@
 #! /usr/bin/env node
 const program = require('commander')
 const path = require('path')
+const { version } = require('../../package.json')
 
 program
-    .version(`Version is ${require(path.resolve(__dirname, '../../package.json')).version}`)
+    .version(version, '-V --version')
     .description('A simple CLI for building initialize project include Wechat applet, Vue, Egg (nodejs)')
     .usage('<command> [options]')
     .command('create')
