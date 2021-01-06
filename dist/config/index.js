bbb4ac8423286f22f33dbc5a87ae2778411a570b
lqs5858@163.com <365858>
2021-01-06 18:40:43 +0800

    1.0.7

--- dist/config/index.js
+++ dist/config/index.js
@@ -4,5 +4,5 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.gitPath = void 0;
-const gitPath = 'LQS5858/express-12306';
+const gitPath = 'LQS5858/express-awilix-templates';
 exports.gitPath = gitPath;
