1d7234beadaf1c12bfa94052be6ce4745aebdb17
E0421 <LQS5858@163.COM>
2023-01-06 11:24:35 +0800

    remove dist

--- dist/config/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.gitPath = void 0;
-const gitPath = 'LQS5858/express-awilix-templates';
-exports.gitPath = gitPath;
