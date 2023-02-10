7d684491632459b975c2bbd737e19faaa887531c
E0421 <LQS5858@163.COM>
2023-02-10 16:15:48 +0800

    update prompts

--- src/lib/create.js
+++ src/lib/create.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer')
 import prompts from 'prompts'
 const { downloadTemplate } = require('./downloadTemplate')
-const { gitPathVue3PcReactive, gitVue3h5Reactive } = require('../config')
+const { gitPathVue3PcReactive, version, gitVue3h5Reactive } = require('../config')
 const path = require('path')
 const logger = require('./logger')
 const { updateJsonFile } = require('../utils/updateJson')
@@ -11,7 +11,7 @@ export const create = (proName) => {
     const env = process.env.NODE_ENV
     const questions = [
         {
-            name: 'projectName',
+            name: 'name',
             type: 'text',
             message: 'Project name:',
             initial: proName ? proName : "vue3-reactive"
@@ -27,14 +27,16 @@ export const create = (proName) => {
             initial: 0
         },
         {
+            type: "text",
             name: 'version',
             message: 'project version',
-            initial: '1.0.0',
+            initial: version,
+            version
         }
     ]
     prompts(questions).then(answer => {
         logger.info('answer', answer)
-        const { projectName: name, gitPath } = answer || {}
+        const { name, gitPath } = answer || {}
         downloadTemplate(gitPath, name).then(() => {
             const filePath = path.resolve(process.cwd(), `./${name}/package.json`)
             updateJsonFile(filePath, answer)
--- src/utils/updateJson.js
+++ src/utils/updateJson.js
@@ -7,11 +7,11 @@ export const updateJsonFile = (fileName, obj) => {
             const data = fs.readFileSync(fileName).toString();
             // 转为json对象
             let json = JSON.parse(data);
-            console.log('json', json);
             // 将用户输入的更新到模板package.json文件
             Object.keys(obj).forEach(key => {
                 json[key] = obj[key];
             });
+            console.log('json', json);
             // 重写模板下的package.json文件
             fs.writeFileSync(fileName, JSON.stringify(json, null, '\t'), 'utf-8');
             resolve();
